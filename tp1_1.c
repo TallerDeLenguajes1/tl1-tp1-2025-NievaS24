8d9c77b6251bc7da6e305b868fb9bc6b586968d2
NievaS24 <santinieva24@gmail.com>
2025-03-28 00:01:48 -0300

    segundo commit punteros en C

--- tp1_1.c
+++ tp1_1.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hola Mundo!\n");
-    return 0;
+    int valor;
+    int *pValor;
+    pValor = &valor;
+    printf("Contenido del puntero: %p\n", pValor);
+    printf("Direccion de memoria almacenada por el puntero: %p\n", pValor);
+    printf("Direccion de memoria de la variable: %p\n", &valor);
+    printf("Direccion de memoria del puntero: %p\n", &pValor);
+    printf("Tamaño de memoria utilizado por la variable %zu", sizeof(int));
 }
